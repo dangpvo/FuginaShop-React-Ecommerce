90e15c83bca8d3de268cc44253345eb45056ad52
Dennis Vo <dennisphuc@gmail.com>
2025-03-30 20:57:43 +0200

    feat: add check out button in cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -91,6 +91,11 @@ const Cart = () => {
       <div className="flex justify-end my-20">
         <div className="w-full sm:w-[450px]">
           <CartTotal></CartTotal>
+          <div className="w-full text-end">
+            <button className="bg-black text-white text-sm my-8 px-8 py-3 cursor-pointer">
+              CHECKOUT
+            </button>
+          </div>
         </div>
       </div>
     </div>
