2d06337c6302ce5ef6e4aeba0cfc4adc0b44318a
Dennis Vo <dennisphuc@gmail.com>
2025-03-30 21:24:14 +0200

    feat: finish the place order page

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -72,6 +72,7 @@ import contact_img from "./contact_img.png";
 import razorpay_logo from "./razorpay_logo.png";
 import stripe_logo from "./stripe_logo.png";
 import cross_icon from "./cross_icon.png";
+import momo_icon from "./momo-icon.png";
 
 export const assets = {
   logo,
@@ -92,6 +93,7 @@ export const assets = {
   razorpay_logo,
   stripe_logo,
   cross_icon,
+  momo_icon,
 };
 
 export const products = [
