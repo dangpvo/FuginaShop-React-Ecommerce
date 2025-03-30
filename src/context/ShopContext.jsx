cd29a6bde6e697f9bc1a0fd4b753bfd3b195caf1
Dennis Vo <dennisphuc@gmail.com>
2025-03-30 20:27:04 +0200

    feat: working on Cart page
    finish counting quantity with size and item, remove item and show item

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -53,6 +53,14 @@ const ShopContextProvider = (props) => {
     return totalCount;
   };
 
+  const updateQuantity = async (itemId, size, quantity) => {
+    let cartData = structuredClone(cartItems);
+
+    cartData[itemId][size] = quantity;
+
+    setCartItems(cartData);
+  };
+
   const value = {
     products,
     currency,
@@ -64,6 +72,7 @@ const ShopContextProvider = (props) => {
     cartItems,
     addToCart,
     getCartCount,
+    updateQuantity,
   };
 
   return (
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,93 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "../components/Title";
+import { assets } from "../assets/assets";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const { products, currency, cartItems, updateQuantity } =
+    useContext(ShopContext);
+
+  const [cartData, setCartData] = useState([]);
+
+  useEffect(() => {
+    const tempData = [];
+
+    for (const items in cartItems) {
+      for (const size in cartItems[items]) {
+        if (cartItems[items][size] > 0) {
+          tempData.push({
+            _id: items,
+            size: size,
+            quantity: cartItems[items][size],
+          });
+        }
+      }
+    }
+
+    setCartData(tempData);
+  }, [cartItems]);
+
+  return (
+    <div className="border-t pt-14">
+      <div className="text-2xl mb-3">
+        <Title text1={"YOUR"} text2={"CART"}></Title>
+      </div>
+      <div>
+        {cartData.map((item, index) => {
+          const prodItem = products.find((pItem) => pItem._id === item._id);
+          return (
+            <div
+              key={index}
+              className="py-4 border-t border-b border-t-gray-300 border-b-gray-300 text-gray-700 grid grid-cols-[4fr_0.5fr_0.5fr] sm:grid-cols-[4fr-2fr-0.5fr] items-center gap-4"
+            >
+              <div className="flex items-start gap-6">
+                <img
+                  className="w-16 sm:w-20"
+                  src={prodItem.image[0]}
+                  alt={prodItem.name}
+                />
+                <div>
+                  <p className="text-xs sm:text-lg font-medium">
+                    {prodItem.name}
+                  </p>
+                  <div className="flex items-center gap-5 mt-2">
+                    <p>
+                      {currency}
+                      {prodItem.price}
+                    </p>
+                    <p className="px-2 sm:px-3 sm:py-1 border border-gray-400 bg-slate-50">
+                      {item.size}
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <input
+                className="border border-gray-400 max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                type="number"
+                min={1}
+                defaultValue={item.quantity}
+                onChange={(e) =>
+                  e.target.value === "" || e.target.value === "0"
+                    ? null
+                    : updateQuantity(
+                        item._id,
+                        item.size,
+                        Number(e.target.value)
+                      )
+                }
+              />
+              <img
+                onClick={() => updateQuantity(item._id, item.size, 0)}
+                className="w-4 mr-4 cursor-pointer"
+                src={assets.bin_icon}
+                alt=""
+              />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
